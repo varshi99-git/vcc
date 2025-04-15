a531f8457ab5f9ac3d463e4365e7a1dd948773da
varshi99-git <g24ai1049@iitj.ac.in>
2025-04-16 00:01:43 +0530

    Update app.py

--- American_Airlines_VCC/app.py
+++ American_Airlines_VCC/app.py
@@ -432,6 +432,7 @@ def admin_logout():
     flash("You have been logged out!")
     return redirect('/admin/login')
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True)
+    app = Flask(__name__)#static_folder="C:\Users\saiva\OneDrive\Desktop\American_Airlines_VCC""
+
